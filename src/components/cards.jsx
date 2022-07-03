5775275e2699d0944fed74932d376c3319ac009f
CH M Anees Hussain Toor <61848047+anees-hussain@users.noreply.github.com>
2022-07-03 08:29:31 +0500

    Added Product Quantity Button in a Product Card

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,26 +1,66 @@
 import React, { Component } from "react";
+
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-import img from '../assets/MR-Future-Products-2020-2.webp'
 import { BsSuitHeart } from "react-icons/bs";
 
+import img from "../assets/MR-Future-Products-2020-2.webp";
+
 class Cards extends Component {
-  state = {};
+  state = {
+    qty: 0,
+  };
+
   render() {
-    const props = this.props;
+    const { productName } = this.props;
+    const { qty } = this.state;
 
     return (
       <>
         <Card style={{ width: "18rem" }}>
           <Card.Img variant="top" src={img} />
           <Card.Body>
-            <Card.Title>{props.productName}</Card.Title>
+            <Card.Title>{productName}</Card.Title>
+
             <Card.Text>Product Description</Card.Text>
+
             <Button variant="warning" style={{ margin: "1px" }}>
               Buy Now
             </Button>
-            <Button variant="danger">{<BsSuitHeart />}</Button>
+
+            <Button variant="danger" style={{ marginLeft: "5px" }}>
+              {<BsSuitHeart />}
+            </Button>
+
+            <Button
+              variant="danger"
+              size="sm"
+              style={{ marginLeft: "5px" }}
+              onClick={() =>
+                this.state.qty > 0
+                  ? this.setState({ qty: this.state.qty - 1 })
+                  : this.setState({ qty: this.state.qty })
+              }
+            >
+              -
+            </Button>
+
+            <input
+              size={1}
+              style={{ marginLeft: "5px" }}
+              value={qty}
+              onChange={(e) => this.setState({ qty: e.target.value })}
+            />
+
+            <Button
+              variant="success"
+              size="sm"
+              style={{ marginLeft: "5px" }}
+              onClick={() => this.setState({ qty: this.state.qty + 1 })}
+            >
+              +
+            </Button>
           </Card.Body>
         </Card>
       </>
